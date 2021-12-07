addb3ca64b9e36e0de5bfca1e0bfb97f3af9f43b
danieljunek15 <danieljunek93@gmail.com>
2021-12-07 16:37:15 +0100

    new migration files added

---
+++ database/migrations/2021_12_07_151029_companie.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Companie extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id('id')->index();
+            $table->string('name')->unique();
+            $table->longText('url')->unique();
+            $table->double('latitude', 15, 5);
+            $table->double('longitude', 15, 5);
+            $table->integer('tags_id');
+            $table->json('software_skils');
+            $table->string('blacklisted');
+            $table->string('email')->unique();
+            $table->string('postal_code')->unique();
+            $table->string('street');
+            $table->integer('address_number');
+            $table->string('province');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
